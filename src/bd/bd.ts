69d29904e75a37222da9046222807403897b9b3b
mact200590 <mact200590@gmail.com>
2020-05-01 17:07:46 -0400

    conection-knex

--- src/bd/bd.ts
+++ src/bd/bd.ts
@@ -1,30 +1,32 @@
-// import mysql from "mysql";
-// import Knex from "knex";
-// const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT,DB_CLIENT } = process.env;
-// // Deprecated!!!! Please, do not use this, only use createConnection to inject it to Context
-// // export const pool = mysql.createPool({
-// //   host: DB_HOST,
-// //   user: DB_USER,
-// //   password: DB_PASSWORD,
-// //   database: DB_NAME,
-// //   port: DB_PORT ? Number.parseInt(DB_PORT) : undefined
-// // });
+import * as Knex from "knex";
 
-// const knex= require('knex')({
-//     client: DB_CLIENT,
-//     connection: {
-//       host :DB_HOST,
-//       user : DB_USER,
-//       password : DB_PASSWORD,
-//       database : DB_NAME
-//     }
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
+
+// export const connectionDB=()=> {
+//   return Knex({
+//     client: "mysql",
+//     //version: "5.7",
+//     connection:  {
+//       host: DB_HOST,
+//       user: DB_USER,
+//       password: DB_PASSWORD,
+//       database: DB_NAME,
+//       port: DB_PORT ? Number.parseInt(DB_PORT) : undefined
+//    }
 //   });
+// }
 
-  // export default knex;
-
-
-
-
+export const knex = require('knex')({
+  client: "mysql",
+  //version: "5.7",
+  connection:  {
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT ? Number.parseInt(DB_PORT) : undefined
+  }
+});
 
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
 
 import { apolloServerInstance } from "./server/apolloServer";
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-//const { ApolloServer, gql } = require('apollo-server');
-//const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
-apolloServerInstance.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+apolloServerInstance.listen(process.env.PORT || 4000).then(({ url }) => {
+  console.log(`env`+process.env.BD_CLIENT),
+  console.log("🚀  Server is running on %o", url);
 });
 
-
-
-
-
-// import { log } from "./utils/logging";
-// import CronModel from "./model/cronjob.model";
-
-// apolloServerInstance.listen(process.env.PORT || 4000).then(({ url }) => {
-//   log("🚀  Server is running on %o", url);
-// });
-
-// // cron services
-// if (
-//   process.env.NODE_ENV === "production" ||
-//   process.env.NODE_ENV === "staging"
-// ) {
-//   const cron = new CronModel();
-//   cron.initLinkUserCron();
-// }
